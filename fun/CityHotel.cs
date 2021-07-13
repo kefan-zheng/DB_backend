ee7d0f2736d4457be4aa38c2c144fc9ec49f3ef8
Dear_西西弗 <73422456+Dear-xxf@users.noreply.github.com>
2021-07-14 05:26:22 +0800

    部分url

---
+++ fun/CityHotel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using LvDao.Models;
+using LvDao;
+using Microsoft.Extensions.Configuration;
+using SqlSugar;
+using System.IO;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LvDao.fun
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("any")]
+    public class CityHotelController : ControllerBase
+    {
+        // GET: api/CityHotel
+        [HttpGet("{city}")]
+        public async Task<ActionResult<IEnumerable<LD_HOTEL>>> CityHotel(string city)
+        {
+            SqlSugar c = new();
+            var db = c.GetInstance();
+            var res = await db.Queryable<LD_HOTEL>().Where(it => it.HLOCATION.Contains(city)).ToListAsync();
+            if (res == null)
+            {
+                return NotFound();
+            }
+            return res;
+        }
+    }
+}
---
+++ fun/SortByStar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using LvDao.Models;
+using LvDao;
+using Microsoft.Extensions.Configuration;
+using SqlSugar;
+using System.IO;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LvDao.fun
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("any")]
+    public class SortByStar : Controller
+    {
+        [HttpGet]
+        public List<dynamic> Sort()
+        {
+            SqlSugar c = new();
+            var db = c.GetInstance();
+            var query = db.Queryable<LD_HOTEL>().OrderBy(q => q.STAR,OrderByType.Desc).ToList();
+            List<dynamic> res = new List<dynamic>();
+            for (int i = 0; i < query.Count; i++)
+            {
+                res.Add(query[i]);
+            }
+            return res;
+        }
+    }
+}
