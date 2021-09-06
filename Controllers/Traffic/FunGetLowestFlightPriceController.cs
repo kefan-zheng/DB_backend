c5774995772d9d000d1ef3f7b66a0af7a0de2833
Hangyu Li <71417771+haha-ykt@users.noreply.github.com>
2021-09-06 11:09:06 +0800

    查询最低价格机票信息

---
+++ Controllers/Traffic/FunGetLowestFlightPriceController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LvDao.Models;
+using Microsoft.AspNetCore.Cors;
+using SqlSugar;
+using LvDao.Controllers;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LvDao.Controllers
+{
+    [EnableCors("any")]
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class FunGetLowestFlightPriceController : ControllerBase
+    {
+        [HttpGet("{fromWhere_toWhere}")]
+        public List<dynamic> GetMomentInfoById(string fromWhere_toWhere)
+        {
+            //处理字符串
+            string[] para = fromWhere_toWhere.Split(new char[] { '&' });
+            string fromWhere = para[0];
+            string toWhere = para[1];
+
+            SqlSugar c = new();
+            var db = c.GetInstance();
+            var table = db.Queryable<LD_TRAFFIC_TICKET, LD_VEHICLE_INFO>((t,v) => new JoinQueryInfos(
+             JoinType.Inner, t.VEHICLE_ID == v.VEHICLE_ID ))
+             .Select((t,v) => new{
+                v.VEHICLE_ID,
+                v.COMPANY_NAME,
+                t.PRICE,
+                v.START_AIRPORT,
+                v.END_AIRPORT,
+                v.START_TIME,
+                v.END_TIME,
+                v.START_LOCATION,
+                v.END_LOCATION,
+                t.REMAINING_NUM,
+                t.SEAT_TYPE})
+             .MergeTable()
+             .Where(it => it.START_LOCATION == fromWhere && it.END_LOCATION == toWhere)
+              .Select(it => new
+              {
+                  it.PRICE,
+                  it.VEHICLE_ID,
+                  it.COMPANY_NAME,
+                  it.START_AIRPORT,
+                  it.END_AIRPORT,
+                  it.START_TIME,
+                  it.END_TIME,
+                  it.SEAT_TYPE,
+                  it.REMAINING_NUM
+              })
+             .OrderBy(it => it.PRICE, OrderByType.Asc)
+             .ToList();
+
+            List<dynamic> res = new();
+            int lowest = table[0].PRICE;
+            for (int i = 0; i < table.Count; i++)
+            {
+                if (table[i].PRICE == table[0].PRICE)
+                    res.Add(table[i]);
+                else
+                    break;
+            }
+            return res;
+        }
+    }
+}
