c6805ca681419b7842715c60c8e0f78b4d071bff
Hangyu Li <71417771+haha-ykt@users.noreply.github.com>
2021-09-06 11:31:16 +0800

    火车票订单

---
+++ Models/LD_PURCHASE_TRAIN_TICKET.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SqlSugar;
+
+namespace LvDao.Models
+{
+    public class LD_PURCHASE_TRAIN_TICKET
+    {
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public string VEHICLE_ID { set; get; }
+
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public string USER_ID { set; get; }
+        public string SEAT_TYPE { set; get; }
+        public int ORDER_AMOUNT { set; get; }
+        public string ORDER_TIME { set; get; }
+        public string TELEPHONE { get; set; }
+    }
+}
